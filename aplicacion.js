ea373adba85122b50af220eec298fdb8dd64fe08
manuellacarra <66756616+manuellacarra@users.noreply.github.com>
2020-07-30 19:40:53 -0300

    Add files via upload

--- aplicacion.js
+++ aplicacion.js
@@ -1,6 +1,28 @@
 
-var nombre = prompt ("Ingrese su nombre");
-var apellido = prompt ("Ingrese su apellido");
+
+var estoyLogueado
+var usuario = "Manuel"
+var capchaOk = false;
+
+var numero = prompt("Ingrese un número");
+
+if (numero >= 10 && numero <= 50){
+	alert("Ingresaste un número entre el 10 y el 50")
+} 
+else{
+  console.error("El número no está entre el 10 y el 50")
+}
+
+
+
+
+
+
+
+
+
+/*var apellido = prompt ("Ingrese su apellido");
 var nombreYApellido = nombre +" "+ apellido;
 
-alert( "Hola" +" "+ nombreYApellido);
+alert( "Hola" +" "+ nombreYApellido);*/
+
