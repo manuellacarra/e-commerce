ae51919cd832b7090e859f5019c8bea71ef1fb85
manuellacarra <66756616+manuellacarra@users.noreply.github.com>
2020-08-04 21:13:19 -0300

    Add files via upload

--- aplicacion.js
+++ aplicacion.js
@@ -1,9 +1,78 @@
 
 
-var estoyLogueado
-var usuario = "Manuel"
-var capchaOk = false;
+ 
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo", "Lunes"];
 
+function listarDias (){
+	for (var i=0; i <= dias.length; i++){
+		if (i==7){
+			alert("Llegaste al día 7")
+		}
+ 		if (i%2){
+ 			console.warn("Es par");
+ 		}
+ 		console.log(dias[i]);
+ 	}
+} 
+
+
+
+
+/*
+
+SWITCH
+switch(i){
+ 			case 2:
+            	console.log("Es par");
+            	break;
+        	case 4:
+            	console.log("Es par");
+            	break;
+        	case 6:
+            	console.log("Es par");
+            	break;      
+         	case 8:
+            	console.log("Es par");
+            	break;
+ 		}
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+CONTINUE Y BREAK
+function listarDias (){
+	for (var i=0; i<7; i++) {	
+		if (i==5){
+			continue; O break; ---> saltea uno o termina ahí 
+		}
+		console.log(dias[i]);
+	}
+}
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+ARRAY
+var dias = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+console.log(dias[4])
+
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+FUNCTION
+var r
+ var n1 = prompt("Ingrese un número: ")
+ var n2 = prompt("Ingrese otro número: ")
+
+ function sumar () {
+ 	n1 = parseInt(n1)
+ 	n2 = parseInt(n2)
+ 	r = n1 + n2
+
+ 	alert("El resultado es: " + r)
+ }
+
+sumar ();
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+
+IF STATEMENT:
 var numero = prompt("Ingrese un número");
 
 if (numero >= 10 && numero <= 50){
@@ -12,17 +81,13 @@ if (numero >= 10 && numero <= 50){
 else{
   console.error("El número no está entre el 10 y el 50")
 }
-
-
-
-
-
-
-
-
-
-/*var apellido = prompt ("Ingrese su apellido");
+///////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+ALERT Y PROMPT:
+var apellido = prompt ("Ingrese su apellido");
 var nombreYApellido = nombre +" "+ apellido;
 
-alert( "Hola" +" "+ nombreYApellido);*/
+alert( "Hola" +" "+ nombreYApellido);
+
+*/
 
