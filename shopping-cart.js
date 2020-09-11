ad314c267e3ad3dd9b3afbeb07dee579033bd556
manuellacarra <66756616+manuellacarra@users.noreply.github.com>
2020-09-11 19:18:38 -0300

    Add files via upload

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,21 +1,4 @@
 
-//SE AGREGAN LA CANTIDAD DE ÍTEMS AL CARRITO
-
-var container = d.getElementsByClassName("container");
-
-function addToCart(event) {
-  let counter = document.getElementById("cartCounter");
-  let currentValue = counter.innerHTML;
-  counter.innerHTML = parseInt(currentValue) + 1;
-
-  let productCard = document.createElement('div');
-  productCard.innerHTML=producto.name;
-
-  productsOnCard.appendChild(productCard); 
-
-}
-
-
 // MOSTRAR/OCUKTAR LISTA DE ELEMENTOS
 const cartIcon = document.getElementById('cartIcon')
 const productCardMask = document.getElementById("productCardMask")
@@ -30,3 +13,39 @@ productCardMask.addEventListener('click', () =>{
 	productCardMask.style.visibility='hidden';
 	productsOnCard.style.visibility='hidden'; 
 })
+
+
+//SE AGREGAN LA CANTIDAD DE ÍTEMS AL CARRITO
+
+var container = d.getElementsByClassName("container");
+
+function addToCart(event) {
+  let counter = document.getElementById("cartCounter");
+  let currentValue = counter.innerHTML;
+  counter.innerHTML = parseInt(currentValue) + 1;
+
+  let productCard = document.createElement('div');
+  productCard.innerHTML = producto.name;
+  /*`
+  		<div class="container">
+         <div class="row">
+           <div class="col shop-detail text-center">
+             <img src=${producto.imgPath}>
+          </div>
+          <div class="col shop-detail mb-4">
+             <p>${producto.name}</p>
+             <p>$${producto.price}</p>
+             <span>Remover</span>
+          </div>
+          <div class="col shop-detail align-middle">
+             <i class="fas fa-chevron-up"></i>
+             <p id="productAmount">0</p>
+             <i class="fas fa-chevron-down"></i>
+          </div>
+        </div>
+  `;*/
+
+  productsOnCard.appendChild(productCard); 
+
+}
+
